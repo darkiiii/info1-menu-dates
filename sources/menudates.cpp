7424faab1cdd127ecba84cc536dba49aef5198ab
Lucas Delolme <52256277+darkiiii@users.noreply.github.com>
2021-12-13 11:20:17 +0100

    ajout de commentaires

--- sources/menudates.cpp
+++ sources/menudates.cpp
@@ -15,30 +15,42 @@ ostream& operator<< (ostream& os, const Date& d)
 // Equivalent de la fonction Saisie : surchage de cin
 istream& operator>> (istream& is, Date& d)
 {
+	// Variable pour stocker l'entrée de l'utilisateur
 	char in[11];
+	// On lit la chaine entrée par l'utilisateur
 	is >> in;
+	// On initialise la variable d
 	d = Date{ 0, 0, 0 };
 
+
+	// ANALYSE DE LA CHAINE
 	int status = 0;
 	int i = 0;
 	int j = 0;
 	while( in[i] != '\0' && i < 11 ) {
+		// Si le charactère est : on passe à l'entrée suivante entre jour/mois/année
 		if (in[i] == ':') {
 			j = 0;
 			status++;
 		}
+		// Sinon on enregistre le charactère dans la bonne variable
 		else {
 			switch (status)
 			{
 			case 0:
+				// Si on a déjà enregistré un nombre pour cette variable, on la multiplie par 10 car c'est le chiffre des 10aines ou plus
 				if (j != 0) d.j *= 10;
+				// Puis on ajoute le nouveau chiffre lu
+				// le -48 c'est car le code ascii des chiffres c'est chiffre + 48
 				d.j += in[i] - 48;
 				break;
 			case 1:
+				// La même chose qu'au dessus
 				if (j != 0) d.m *= 10;
 				d.m += in[i] - 48;
 				break;
 			case 2:
+				// Ici avec les années ça marche parreil mais on se retrouve à passer plusieurs fois (jusqu'à 4 normalement)
 				if (j != 0) d.a *= 10;
 				d.a += in[i] - 48;
 				break;
@@ -53,21 +65,26 @@ istream& operator>> (istream& is, Date& d)
 
 // Equivalent de la fonction ajouter : surcharge de l'opérateur +
 Date operator+(Date const& d1, Date const& d2) {
+	// La date de sortie prend la valeur de la date 1
 	Date ds = d1;
 
+	// On ajoute les années et les jours
 	ds.a += d2.a;
 	ds.j += d2.j;
 
+	// On ajoute le bon nombre d'années si le nombre de jours est supérieur à 365/366
 	while (ds.j > (365 + (Bisextile(ds.a) ? 1 : 0))) {
 		ds.j -= (365 + (Bisextile(ds.a) ? 1 : 0));
 		ds.a += 1;
 	}
 
+	// On ajoute le bon nombre des mois si le nombre de jours ne rentre pas dans le mois
 	while (ds.j > MOIS[ds.m - 1] + ((Bisextile(ds.a) && ds.m == 2) ? 1 : 0)) {
 		ds.j -= MOIS[ds.m - 1] + ((Bisextile(ds.a) && ds.m == 2) ? 1 : 0);
 		ds.m += 1;
 	}
 
+	// On ajoute le nombre de mois (effectivement c'est pas parfait mais c'était trop long sinon)
 	ds.m += d2.m;
 
 	ds.a += ds.m / 12;
@@ -97,21 +114,6 @@ int PuissanceDe10(int i) {
 */
 bool Bisextile(int a) {
 	return ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0 && a % 4000 != 0));
-}
-
-/*
-* R : Converti une chaine de charactère en une date
-* E : une chaine de charactères
-* S : une date
-*/
-Date Convertir(const char str[11]) {
-	Date d;
-
-	d.j = (str[0] - 48) * 10 + (str[1] - 48);
-	d.m = (str[3] - 48) * 10 + (str[4] - 48);
-	d.a = (str[6] - 48) * 1000 + (str[7] - 48) * 100 + (str[8] - 48) * 10 + (str[9] - 48);
-
-	return d;
 }
 
 /*
